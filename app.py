6781ee0c08b2017fff4972f515eb0eb516ad19b0
Romain Brogniez <romainbrogniez@MacBook-Pro-de-Romain.local>
2025-04-10 11:09:09 +0200

    Ports configuration

--- app.py
+++ app.py
@@ -3,19 +3,15 @@ import qrcode
 
 app = Flask(__name__)
 
-# Route pour la page d'accueil
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # Récupérer le texte du formulaire
         data = request.form["data"]
-        # Générer le QR code
         qr = qrcode.make(data)
-        # Sauvegarder l'image dans un fichier
         qr.save("static/qrcode.png")
         return render_template("index.html", qr_code="static/qrcode.png")
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # On ne démarre pas le serveur ici, car il sera démarré par Gunicorn.
-    pass
+    # Important: Assurez-vous que l'application écoute sur toutes les interfaces
+    app.run(host='0.0.0.0', port=10000)  # Port par défaut sur Render
