35b3823726063e9ca51ba4005fcef9306348d9fd
Romain Brogniez <romainbrogniez@MacBook-Pro-de-Romain.local>
2025-04-10 10:53:20 +0200

    Modification of run.py

--- run.py
+++ run.py
@@ -1,21 +1,6 @@
-import webbrowser
-from time import sleep
-from threading import Thread
-from app import app  # Assure-toi que ton app Flask est dans app.py
+from app import app
 
-# Fonction pour démarrer le serveur Flask dans un thread
-def run_flask():
-    app.run(debug=True, host='127.0.0.1', port=5000)
-
-# Démarrer le serveur Flask dans un thread séparé
-flask_thread = Thread(target=run_flask)
-flask_thread.start()
-
-# Attendre un peu que le serveur Flask se lance
-sleep(2)
-
-# Ouvrir le navigateur par défaut pour l'application
-webbrowser.open("http://127.0.0.1:5000/")
-
-# Empêcher le script de se terminer immédiatement
-flask_thread.join()
+if __name__ == "__main__":
+    # Si l'on exécute ce fichier localement, ne démarrons pas Flask en mode développement.
+    # Le serveur de production Gunicorn prendra en charge le démarrage sur Render.
+    pass
